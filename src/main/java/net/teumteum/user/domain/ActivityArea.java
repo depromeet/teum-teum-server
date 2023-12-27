9f573f2ce14aa5affc86e419d0f827fb53b4b735
devxb <dlwnsdud205@naver.com>
2023-12-27 14:20:01 +0900

    refactor: street에 기본생성자를 추가한다

--- src/main/java/net/teumteum/user/domain/ActivityArea.java
+++ src/main/java/net/teumteum/user/domain/ActivityArea.java
@@ -4,6 +4,7 @@ package net.teumteum.user.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,5 +20,5 @@ public class ActivityArea {
     private String city;
 
     @ElementCollection
-    private List<String> street;
+    private List<String> street = new ArrayList<>();
 }
