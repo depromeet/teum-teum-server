8e7cbcc1e5bf5025b36251e28b79071217bb2519
ddingmin <ddingminyt@gmail.com>
2023-12-31 16:13:35 +0900

    refactor: MeetingRepository 응답 객체 변경

--- src/main/java/net/teumteum/meeting/domain/MeetingRepository.java
+++ src/main/java/net/teumteum/meeting/domain/MeetingRepository.java
@@ -1,16 +1,16 @@
 package net.teumteum.meeting.domain;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
-    Page<Meeting> findByPromiseDateTimeGreaterThanOrderByIdDesc(Pageable size, LocalDateTime promiseDateTime);
+    List<Meeting> findByPromiseDateTimeGreaterThanOrderByIdDesc(Pageable size, LocalDateTime promiseDateTime);
 
-    Page<Meeting> findByIdLessThanEqualAndPromiseDateTimeGreaterThanOrderByIdDesc(Long id, Pageable size, LocalDateTime promiseDateTime);
+    List<Meeting> findByIdLessThanEqualAndPromiseDateTimeGreaterThanOrderByIdDesc(Long id, Pageable size, LocalDateTime promiseDateTime);
 
 }
