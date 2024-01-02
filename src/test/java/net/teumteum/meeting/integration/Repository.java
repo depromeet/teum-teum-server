f58f25b51d4a89029025b431f49d48b1d955092d
ddingmin <ddingminyt@gmail.com>
2024-01-02 22:39:33 +0900

    test: 여러 조건에 따른 미팅 목록을 조회를 테스트한다.
    - WebTestClient가 WebFlux 기반의 라이브러리로 Pageable의 값을 Resolving 하는 리솔버가 작동하지 않아 커스텀 리솔버를 테스트 환경에 추가했습니다.
    - [ref](https://medium.com/@davidpetro/spring-webflux-and-pageable-be55104c234f)

--- src/test/java/net/teumteum/meeting/integration/Repository.java
+++ src/test/java/net/teumteum/meeting/integration/Repository.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import net.teumteum.meeting.domain.Meeting;
 import net.teumteum.meeting.domain.MeetingFixture;
 import net.teumteum.meeting.domain.MeetingRepository;
+import net.teumteum.meeting.domain.Topic;
 import org.springframework.boot.test.context.TestComponent;
 
 import java.util.List;
@@ -18,12 +19,62 @@ class Repository {
         return meetingRepository.saveAndFlush(meeting);
     }
 
-    List<Meeting> saveAndGetOpenMeetings(int size) {
-        var meetings = Stream.generate(MeetingFixture::getOpenMeeting)
+    List<Meeting> saveAndGetOpenMeetingsByTopic(int size, Topic topic) {
+        var meetings = Stream.generate(() -> MeetingFixture.getOpenMeetingWithTopic(topic))
                 .limit(size)
                 .toList();
         return meetingRepository.saveAllAndFlush(meetings);
     }
+
+    List<Meeting> saveAndGetCloseMeetingsTopic(int size, Topic topic) {
+        var meetings = Stream.generate(() -> MeetingFixture.getCloseMeetingWithTopic(topic))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetOpenMeetingsByTitle(int size, String title) {
+        var meetings = Stream.generate(() -> MeetingFixture.getOpenMeetingWithTitle(title))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetCloseMeetingsByTitle(int size, String title) {
+        var meetings = Stream.generate(() -> MeetingFixture.getCloseMeetingWithTitle(title))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetOpenMeetingsByIntroduction(int size, String introduction) {
+        var meetings = Stream.generate(() -> MeetingFixture.getOpenMeetingWithIntroduction(introduction))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetCloseMeetingsByIntroduction(int size, String introduction) {
+        var meetings = Stream.generate(() -> MeetingFixture.getCloseMeetingWithIntroduction(introduction))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetOpenMeetingsByParticipantUserId(int size, Long participantUserId) {
+        var meetings = Stream.generate(() -> MeetingFixture.getOpenMeetingWithParticipantUserId(participantUserId))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
+    List<Meeting> saveAndGetCloseMeetingsByParticipantUserId(int size, Long participantUserId) {
+        var meetings = Stream.generate(() -> MeetingFixture.getCloseMeetingWithParticipantUserId(participantUserId))
+                .limit(size)
+                .toList();
+        return meetingRepository.saveAllAndFlush(meetings);
+    }
+
     void clear() {
         meetingRepository.deleteAll();
     }
