8ad3e4d9c1db67cf27d3d97b82e5838c468ee284
choidongkuen <danaver12@daum.net>
2024-01-11 17:35:23 +0900

    refactor: jwtService 불필요한 주석 제거 (#32)

--- src/main/java/net/teumteum/core/security/service/JwtService.java
+++ src/main/java/net/teumteum/core/security/service/JwtService.java
@@ -4,8 +4,8 @@ import io.jsonwebtoken.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.teumteum.auth.domain.response.TokenResponse;
 import net.teumteum.core.property.JwtProperty;
-import net.teumteum.core.security.dto.TokenResponse;
 import net.teumteum.user.domain.User;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Service;
@@ -23,7 +23,6 @@ public class JwtService {
     private final JwtProperty jwtProperty;
     private final RedisService redisService;
 
-    // HttpServletRequest 부터 Access Token 추출
     public Optional<String> extractAccessToken(HttpServletRequest request) {
         return Optional.ofNullable(request.getHeader(this.jwtProperty.getAccess().getHeader()))
                 .filter(StringUtils::hasText)
@@ -31,25 +30,21 @@ public class JwtService {
                 .map(accessToken -> accessToken.replace(jwtProperty.getBearer(), ""));
     }
 
-    // HttpServletRequest 부터 Refresh Token 추출
     public String extractRefreshToken(HttpServletRequest request) {
         return request.getHeader(this.jwtProperty.getRefresh().getHeader());
     }
 
-    // access token 생성
     public String createAccessToken(String payload) {
         return this.createToken(payload, this.jwtProperty.getAccess().getExpiration());
     }
 
 
-    // refresh token 생성
     public String createRefreshToken() {
         return this.createToken(UUID.randomUUID().toString(), this.jwtProperty.getRefresh().getExpiration());
 
     }
 
 
-    // access token 으로부터 회원 아이디 추출
     public String getUserIdFromToken(String token) {
         try {
             return Jwts.parser()
@@ -62,25 +57,17 @@ public class JwtService {
         }
     }
 
-    // kakao oauth 로그인 & 일반 로그인 시 jwt 응답 생성 + redis refresh 저장
     public TokenResponse createServiceToken(User users) {
         String accessToken = this.createAccessToken(String.valueOf(users.getId()));
         String refreshToken = this.createRefreshToken();
 
-        /* 서비스 토큰 생성 */
-        TokenResponse userServiceTokenResponseDto = TokenResponse.builder()
-                .accessToken(this.jwtProperty.getBearer() + " " + accessToken)
-                .refreshToken(refreshToken)
-                .build();
-
-        /* redis refresh token 저장 */
         this.redisService.setDataExpire(String.valueOf(users.getId()),
-                userServiceTokenResponseDto.getRefreshToken(), this.jwtProperty.getRefresh().getExpiration());
+                refreshToken, this.jwtProperty.getRefresh().getExpiration());
 
-        return userServiceTokenResponseDto;
+        return new TokenResponse(this.jwtProperty.getBearer() + " " + accessToken, refreshToken);
+
     }
 
-    // token 유효성 검증
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claimsJws = Jwts.parser().setSigningKey(this.jwtProperty.getSecret()).parseClaimsJws(token);
@@ -95,7 +82,6 @@ public class JwtService {
         return false;
     }
 
-    // 실제 token 생성 로직
     private String createToken(String payload, Long tokenExpiration) {
         Claims claims = Jwts.claims().setSubject(payload);
         Date tokenExpiresIn = new Date(new Date().getTime() + tokenExpiration);
