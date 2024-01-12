6ee62a3ff8118b57753417f3bff78f362ede24fe
choidongkuen <danaver12@daum.net>
2024-01-13 04:54:10 +0900

    feat: 토큰 재발행 관련 AuthController,AuthService 구현 (#40)

---
+++ src/main/java/net/teumteum/auth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package net.teumteum.auth.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import net.teumteum.auth.domain.response.TokenResponse;
+import net.teumteum.auth.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/reissue")
+    @ResponseStatus(HttpStatus.OK)
+    public TokenResponse reissue(HttpServletRequest request) {
+        return authService.reissue(request);
+    }
+}
--- src/main/java/net/teumteum/auth/service/AuthService.java
+++ src/main/java/net/teumteum/auth/service/AuthService.java
@@ -1,9 +1,12 @@
 package net.teumteum.auth.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.teumteum.auth.domain.response.TokenResponse;
 import net.teumteum.core.security.service.JwtService;
+import net.teumteum.core.security.service.RedisService;
 import net.teumteum.user.domain.User;
 import net.teumteum.user.domain.UserConnector;
 import org.springframework.stereotype.Service;
@@ -14,10 +17,43 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final JwtService jwtService;
+    private final RedisService redisService;
     private final UserConnector userConnector;
 
     public Optional<User> findUserByToken(String accessToken) {
         Long id = Long.parseLong(jwtService.getUserIdFromToken(accessToken));
         return userConnector.findUserById(id);
+    }
+
+    public TokenResponse reissue(HttpServletRequest request) {
+        String refreshToken = jwtService.extractRefreshToken(request);
+        String accessToken = jwtService.extractAccessToken(request);
+
+        checkRefreshTokenValidation(refreshToken);
+
+        User user = findUserByToken(accessToken)
+            .orElseThrow(() -> new IllegalArgumentException("access token 에 해당하는 user를 찾을 수 없습니다."));
+
+        checkRefreshTokenMatch(user, refreshToken);
+        return issueNewToken(user);
+    }
+
+    private void checkRefreshTokenValidation(String refreshToken) {
+        if (!jwtService.validateToken(refreshToken)) {
+            throw new IllegalArgumentException("refresh token 이 유효하지 않습니다.");
+        }
+    }
+
+    private void checkRefreshTokenMatch(User user, String refreshToken) {
+        String savedRefreshToken = redisService.getData(String.valueOf(user.getId()));
+        if (!savedRefreshToken.equals(refreshToken)) {
+            throw new IllegalArgumentException("refresh token 이 일치하지 않습니다.");
+        }
+    }
+
+
+    private TokenResponse issueNewToken(User user) {
+        return new TokenResponse(jwtService.createAccessToken(user.getOauth().getOauthId()),
+            jwtService.createRefreshToken());
     }
 }
