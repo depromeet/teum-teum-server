37d55924d4f30376ed5d7ca37fe6068b51adb9e8
choidongkuen <danaver12@daum.net>
2024-01-15 09:54:33 +0900

    refactor: 인증 관련 코드 리팩토링 (#83)

--- src/main/java/net/teumteum/auth/controller/OAuthLoginController.java
+++ src/main/java/net/teumteum/auth/controller/OAuthLoginController.java
@@ -3,19 +3,18 @@ package net.teumteum.auth.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.teumteum.auth.domain.response.TokenResponse;
+import net.teumteum.auth.service.OAuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
-@RequestMapping
 @RequiredArgsConstructor
 public class OAuthLoginController {
 
-    private final net.teumteum.auth.service.OAuthService oAuthService;
+    private final OAuthService oAuthService;
 
     @GetMapping("/logins/callbacks/{provider}")
     @ResponseStatus(HttpStatus.OK)
