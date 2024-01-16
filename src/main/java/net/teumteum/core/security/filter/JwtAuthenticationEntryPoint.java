73d6c2a237794aece029548a4943a60aa9afbcc4
choidongkuen <danaver12@daum.net>
2024-01-17 02:58:59 +0900

    refactor: JWT 및 인증 관련 로직 리팩토링 (#101)

--- src/main/java/net/teumteum/core/security/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/net/teumteum/core/security/filter/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package net.teumteum.core.security.filter;
 
+import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,20 +17,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private static final String ATTRIBUTE_NAME = "exception";
+
     @Override
-    public void commence(HttpServletRequest request,
-        HttpServletResponse response,
-        AuthenticationException authenticationException
-    ) throws IOException {
-        this.sendUnAuthenticatedError(response, authenticationException);
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authenticationException) throws IOException {
+        this.sendUnAuthenticatedError(request, response, authenticationException);
     }
 
-    private void sendUnAuthenticatedError(HttpServletResponse response,
-        Exception exception) throws IOException {
+    private void sendUnAuthenticatedError(HttpServletRequest request, HttpServletResponse response, Exception exception)
+        throws IOException {
+        response.setStatus(SC_UNAUTHORIZED);
         OutputStream os = response.getOutputStream();
         ObjectMapper objectMapper = new ObjectMapper();
         log.error("Responding with unauthenticated error. Message - {}", exception.getMessage());
-        objectMapper.writeValue(os, ErrorResponse.of("인증 과정에서 오류가 발생했습니다."));
+        objectMapper.writeValue(os, ErrorResponse.of((String) request.getAttribute(ATTRIBUTE_NAME)));
         os.flush();
     }
 }
