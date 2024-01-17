d94f4d4237a5f34fe1f891d5a5ba551c33af0d7d
devxb <dlwnsdud205@naver.com>
2024-01-17 14:32:18 +0900

    feat: 예시 코드를 작성한다

---
+++ src/gatling/java/simulation/GetUserSimulation.java
@@ -0,0 +1,27 @@
+package simulation;
+
+import static io.gatling.javaapi.core.CoreDsl.rampUsers;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import protocol.Protocol;
+
+public class GetUserSimulation extends Simulation {
+
+    private final ScenarioBuilder scn = scenario(this.getClass().getSimpleName())
+        .exec(http("get user")
+            .get("/users/1")
+            .check(status().is(200))
+        );
+
+    {
+        setUp(
+            scn.injectOpen(rampUsers(10).during(Duration.of(10, ChronoUnit.MINUTES)))
+        ).protocols(Protocol.httpProtocol);
+    }
+}
