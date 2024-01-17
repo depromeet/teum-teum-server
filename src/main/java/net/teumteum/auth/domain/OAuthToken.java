132a003bfab5cc087d8bc3a20a536088bb81bd19
choidongkuen <danaver12@daum.net>
2024-01-18 06:22:02 +0900

    refactor : 기타 OAuth 관련 로직 리팩토링(#111)

--- src/main/java/net/teumteum/auth/domain/OAuthToken.java
+++ src/main/java/net/teumteum/auth/domain/OAuthToken.java
@@ -4,29 +4,11 @@ package net.teumteum.auth.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record OAuthToken(
-    @JsonProperty("token_type")
-    String tokenType,
     @JsonProperty("access_token")
     String accessToken,
     String scope,
-
-    @JsonProperty("expires_in")
-    Integer expiresIn
+    @JsonProperty("token_type")
+    String tokenType
 ) {
 
-    public String getTokenType() {
-        return this.tokenType;
-    }
-
-    public String getAccessToken() {
-        return this.accessToken;
-    }
-
-    public String getScope() {
-        return this.scope;
-    }
-
-    public Integer getExpiresIn() {
-        return this.expiresIn;
-    }
 }
