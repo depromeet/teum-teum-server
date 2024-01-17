f3b41ffd0cb38d8d72ec7f0fe5bb60292f21a337
choidongkuen <danaver12@daum.net>
2024-01-17 15:24:04 +0900

    refactor: ObjectMapper Autowired 로 주입 (#101)

--- src/main/java/net/teumteum/core/security/filter/JwtAccessDeniedHandler.java
+++ src/main/java/net/teumteum/core/security/filter/JwtAccessDeniedHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.teumteum.core.error.ErrorResponse;
 import org.springframework.security.access.AccessDeniedException;
@@ -15,7 +16,10 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request,
@@ -29,7 +33,6 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
         Exception exception) throws IOException {
         response.setStatus(SC_FORBIDDEN);
         OutputStream os = response.getOutputStream();
-        ObjectMapper objectMapper = new ObjectMapper();
         log.error("Responding with unauthorized error. Message - {}", exception.getMessage());
         objectMapper.writeValue(os, ErrorResponse.of("인가 과정에서 오류가 발생했습니다."));
         os.flush();
--- src/main/java/net/teumteum/core/security/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/net/teumteum/core/security/filter/JwtAuthenticationEntryPoint.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.teumteum.core.error.ErrorResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -15,9 +16,12 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private static final String ATTRIBUTE_NAME = "exception";
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
@@ -29,7 +33,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         throws IOException {
         response.setStatus(SC_UNAUTHORIZED);
         OutputStream os = response.getOutputStream();
-        ObjectMapper objectMapper = new ObjectMapper();
         log.error("Responding with unauthenticated error. Message - {}", exception.getMessage());
         objectMapper.writeValue(os, ErrorResponse.of((String) request.getAttribute(ATTRIBUTE_NAME)));
         os.flush();
