be58b5b80e4697b1f06652bd3af502ea02d94168
xb205 <62425964+devxb@users.noreply.github.com>
2024-02-10 13:28:38 +0900

    feat: 유저가 한달 동안 받은 알람을 조회하는 기능 개발 (#185)
    * fix: flyway schema에 _이 하나밖에 없는 버그를 수정한다
    
    * feat: 유저가 한달동안 받은 알람을 조회하는 기능을 개발한다
    
    * refactor: AlertRepository의 finalAll 메소드를 findAllByCreatedAt으로 변경한다

--- src/main/java/net/teumteum/alert/domain/AlertRepository.java
+++ src/main/java/net/teumteum/alert/domain/AlertRepository.java
@@ -1,7 +1,15 @@
 package net.teumteum.alert.domain;
 
+import java.time.Instant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AlertRepository extends JpaRepository<Alert, Long> {
 
+    List<Alert> findAllByUserId(Long userId);
+
+    @Query("select a from alert as a where a.createdAt <= :createdAt")
+    List<Alert> findAllByCreatedAt(@Param("createdAt") Instant createdAt);
 }
--- src/main/java/net/teumteum/alert/domain/AlertService.java
+++ src/main/java/net/teumteum/alert/domain/AlertService.java
@@ -1,6 +1,10 @@
 package net.teumteum.alert.domain;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
+import net.teumteum.alert.domain.response.AlertsResponse;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,10 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AlertService {
 
+    private static final String EVERY_12AM = "0 0 0 * * *";
+
     private final AlertRepository alertRepository;
 
+    @Transactional
     public Alert save(Alert alert) {
         return alertRepository.save(alert);
     }
 
+    public AlertsResponse findAllByUserId(Long userId) {
+        return AlertsResponse.of(alertRepository.findAllByUserId(userId));
+    }
+
+    @Transactional
+    @Scheduled(cron = EVERY_12AM)
+    public void deleteOneMonthBeforeAlert() {
+        var deleteTargets = alertRepository.findAllByCreatedAt(Instant.now().minus(1, ChronoUnit.MONTHS));
+        alertRepository.deleteAllInBatch(deleteTargets);
+    }
 }
