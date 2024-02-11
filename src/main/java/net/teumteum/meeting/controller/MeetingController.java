c800c99b6626440188cd41455706cfc13687642b
choidongkuen <danaver12@daum.net>
2024-02-11 15:47:40 +0900

    feat: 모임 참여자 정보 조회 API 기능 구현 (#178)

--- src/main/java/net/teumteum/meeting/controller/MeetingController.java
+++ src/main/java/net/teumteum/meeting/controller/MeetingController.java
@@ -9,6 +9,7 @@ import net.teumteum.core.security.service.SecurityService;
 import net.teumteum.meeting.domain.Topic;
 import net.teumteum.meeting.domain.request.CreateMeetingRequest;
 import net.teumteum.meeting.domain.request.UpdateMeetingRequest;
+import net.teumteum.meeting.domain.response.MeetingParticipantsResponse;
 import net.teumteum.meeting.domain.response.MeetingResponse;
 import net.teumteum.meeting.domain.response.MeetingsResponse;
 import net.teumteum.meeting.model.PageDto;
@@ -95,6 +96,12 @@ public class MeetingController {
     public void deleteParticipant(@PathVariable("meetingId") Long meetingId) {
         Long userId = securityService.getCurrentUserId();
         meetingService.cancelParticipant(meetingId, userId);
+    }
+
+    @GetMapping("/{meetingId}/participants")
+    @ResponseStatus(HttpStatus.OK)
+    public List<MeetingParticipantsResponse> getParticipants(@PathVariable("meetingId") Long meetingId) {
+        return meetingService.getParticipants(meetingId);
     }
 
     @PostMapping("/{meetingId}/reports")
