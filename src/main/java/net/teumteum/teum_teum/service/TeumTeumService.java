5a35805f31621c665cb975232f09f8e9550c3398
choidongkuen <danaver12@daum.net>
2024-02-11 13:03:04 +0900

    refactor: TeumTeumService 리팩토링 (#178)

--- src/main/java/net/teumteum/teum_teum/service/TeumTeumService.java
+++ src/main/java/net/teumteum/teum_teum/service/TeumTeumService.java
@@ -9,20 +9,20 @@ import static java.util.Comparator.comparingDouble;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import net.teumteum.core.security.service.RedisService;
 import net.teumteum.teum_teum.domain.UserLocation;
 import net.teumteum.teum_teum.domain.request.UserLocationRequest;
 import net.teumteum.teum_teum.domain.response.UserAroundLocationsResponse;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+
 @Service
 @RequiredArgsConstructor
 public class TeumTeumService {
 
     private static final int SEARCH_LIMIT = 6;
-    private static final long USER_LOCATION_DATA_DURATION = 30L;
+    private static final long USER_LOCATION_DATA_DURATION = 10L;
+    private static final int AROUND_USER_LOCATION_DISTANCE = 100;
 
     private final RedisService redisService;
 
@@ -37,7 +37,7 @@ public class TeumTeumService {
         Set<UserLocation> aroundUserLocations = allUserLocations.stream()
             .filter(userLocation -> !userLocation.id().equals(request.id()))
             .filter(userLocation -> calculateDistance(request.latitude(), request.longitude(),
-                userLocation.latitude(), userLocation.longitude()) <= 100)
+                userLocation.latitude(), userLocation.longitude()) <= AROUND_USER_LOCATION_DISTANCE)
             .sorted(comparingDouble(userLocation
                 -> calculateDistance(request.latitude(), request.longitude(),
                 userLocation.latitude(), userLocation.longitude()))
