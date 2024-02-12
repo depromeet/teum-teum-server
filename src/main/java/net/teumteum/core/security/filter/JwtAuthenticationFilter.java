40fd56bb06cbed95a0ded412db6249d90a6367df
choidongkuen <danaver12@daum.net>
2024-02-12 15:04:29 +0900

    refactor: JwtAuthenticationFilter 불필요한 로직 제거 (#198)

--- src/main/java/net/teumteum/core/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/net/teumteum/core/security/filter/JwtAuthenticationFilter.java
@@ -15,7 +15,6 @@ import net.teumteum.user.domain.User;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -31,7 +30,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProperty jwtProperty;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
+    public void doFilterInternal(HttpServletRequest request,
         HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
         if (request.getMethod().equals("OPTIONS")) {
@@ -73,10 +72,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String resolveTokenFromRequest(HttpServletRequest request) {
         String token = request.getHeader(jwtProperty.getAccess().getHeader());
-        if (!ObjectUtils.isEmpty(token) && token.toLowerCase().startsWith(jwtProperty.getBearer().toLowerCase())) {
+        if (token.toLowerCase().startsWith(jwtProperty.getBearer().toLowerCase())) {
             return token.substring(7);
         }
-        setRequestAttribute(request, "요청에 대한 JWT 파싱 과정에서 문제가 발생했습니다.");
         return null;
     }
 
