b56a9a8f356c3bca358fa40bb73f0e7a6211f2e6
xb205 <62425964+devxb@users.noreply.github.com>
2024-02-12 13:04:03 +0900

    refactor: Instant를 모두 LocalDateTime으로 변경 (#200)

--- src/main/java/net/teumteum/core/entity/TimeBaseEntity.java
+++ src/main/java/net/teumteum/core/entity/TimeBaseEntity.java
@@ -3,6 +3,7 @@ package net.teumteum.core.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -10,20 +11,19 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.Instant;
-
 @Getter
 @SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeBaseEntity {
+
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private Instant createdAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private Instant updatedAt;
+    private LocalDateTime updatedAt;
 }
 
