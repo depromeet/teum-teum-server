914b670071367e9c530ebfedda616a55c591e1b5
devxb <dlwnsdud205@naver.com>
2024-02-12 13:59:38 +0900

    release: 0.4.5

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -100,8 +100,8 @@ create table if not exists alert(
     title text not null,
     `body` text not null,
     type enum('BEFORE_MEETING'),
-    created_at timestamp(6) not null,
-    updated_at timestamp(6) not null,
+    created_at datetime not null,
+    updated_at datetime not null,
     is_read boolean not null,
     primary key (id)
 );
