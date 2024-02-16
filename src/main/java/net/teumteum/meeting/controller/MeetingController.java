8b7fd661698471a935a3cfe72bba4238e105b9b5
choidongkuen <danaver12@daum.net>
2024-02-16 10:25:11 +0900

    refactor: MeetingController getCurrentUserId() 메소드로 통일 (#228)

--- src/main/java/net/teumteum/meeting/controller/MeetingController.java
+++ src/main/java/net/teumteum/meeting/controller/MeetingController.java
@@ -9,7 +9,7 @@ import net.teumteum.core.security.service.SecurityService;
 import net.teumteum.meeting.domain.Topic;
 import net.teumteum.meeting.domain.request.CreateMeetingRequest;
 import net.teumteum.meeting.domain.request.UpdateMeetingRequest;
-import net.teumteum.meeting.domain.response.MeetingParticipantsResponse;
+import net.teumteum.meeting.domain.response.MeetingParticipantResponse;
 import net.teumteum.meeting.domain.response.MeetingResponse;
 import net.teumteum.meeting.domain.response.MeetingsResponse;
 import net.teumteum.meeting.model.PageDto;
@@ -43,14 +43,14 @@ public class MeetingController {
     public MeetingResponse createMeeting(
         @RequestPart @Valid CreateMeetingRequest meetingRequest,
         @RequestPart List<MultipartFile> images) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         return meetingService.createMeeting(images, meetingRequest, userId);
     }
 
     @GetMapping("/{meetingId}")
     @ResponseStatus(HttpStatus.OK)
     public MeetingResponse getMeetingById(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         return meetingService.getMeetingById(meetingId, userId);
     }
 
@@ -64,7 +64,7 @@ public class MeetingController {
         @RequestParam(value = "participantUserId", required = false) Long participantUserId,
         @RequestParam(value = "isBookmarked", required = false) Boolean isBookmarked,
         @RequestParam(value = "searchWord", required = false) String searchWord) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         return meetingService.getMeetingsBySpecification(pageable, topic, meetingAreaStreet, participantUserId,
             searchWord, isBookmarked, isOpen, userId);
     }
@@ -74,55 +74,56 @@ public class MeetingController {
     public MeetingResponse updateMeeting(@PathVariable Long meetingId,
         @RequestPart @Valid UpdateMeetingRequest request,
         @RequestPart List<MultipartFile> images) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         return meetingService.updateMeeting(meetingId, images, request, userId);
     }
 
     @DeleteMapping("/{meetingId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteMeeting(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         meetingService.deleteMeeting(meetingId, userId);
     }
 
     @PostMapping("/{meetingId}/participants")
     @ResponseStatus(HttpStatus.CREATED)
     public MeetingResponse addParticipant(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         return meetingService.addParticipant(meetingId, userId);
     }
 
     @DeleteMapping("/{meetingId}/participants")
     @ResponseStatus(HttpStatus.OK)
     public void deleteParticipant(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         meetingService.cancelParticipant(meetingId, userId);
     }
 
     @GetMapping("/{meetingId}/participants")
     @ResponseStatus(HttpStatus.OK)
-    public List<MeetingParticipantsResponse> getParticipants(@PathVariable("meetingId") Long meetingId) {
-        return meetingService.getParticipants(meetingId);
+    public List<MeetingParticipantResponse> getParticipants(@PathVariable("meetingId") Long meetingId) {
+        Long userId = getCurrentUserId();
+        return meetingService.getParticipants(meetingId, userId);
     }
 
     @PostMapping("/{meetingId}/reports")
     @ResponseStatus(HttpStatus.CREATED)
     public void reportMeeting(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         meetingService.reportMeeting(meetingId, userId);
     }
 
     @PostMapping("/{meetingId}/bookmarks")
     @ResponseStatus(HttpStatus.CREATED)
     public void addBookmark(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         meetingService.addBookmark(meetingId, userId);
     }
 
     @DeleteMapping("/{meetingId}/bookmarks")
     @ResponseStatus(HttpStatus.OK)
     public void deleteBookmark(@PathVariable("meetingId") Long meetingId) {
-        Long userId = securityService.getCurrentUserId();
+        Long userId = getCurrentUserId();
         meetingService.cancelBookmark(meetingId, userId);
     }
 
@@ -131,5 +132,9 @@ public class MeetingController {
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException illegalArgumentException) {
         Sentry.captureException(illegalArgumentException);
         return ErrorResponse.of(illegalArgumentException);
+    }
+
+    private Long getCurrentUserId() {
+        return securityService.getCurrentUserId();
     }
 }
